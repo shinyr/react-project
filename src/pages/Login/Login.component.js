6468f1ae702b5c09b96ab9dbd465cb77e81a8b23
shinyrajrl <shinyr@qburst.com>
2018-07-24 12:49:22 +0530

    login component changes

--- src/pages/Login/Login.component.js
+++ src/pages/Login/Login.component.js
@@ -1,28 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import intl from 'react-intl-universal';
-import styles from './css/styles.css';
+import './css/styles.css';
 
-const LoginComp = ({ login }) => (
-  <div>
-    <h2>{intl.get('menu/login').d('Login')}</h2>
-    <p> </p>
-    <div className={styles.loginWrapper}>
-      <section className="loginBgContainer"></section>
-      <section className="loginContainer">
-          <p> Username</p>
-          <input type="text" placeholder="Username"/>
-          <p> Password</p>
-          <input type="password" placeholder="Password"/>
-          <button> Login </button>
-      </section>
-    </div>
-  </div>
-);
+class LoginComp extends React.Component {
+    constructor(props) {
+        super(props);
 
-LoginComp.propTypes = {
-  login: PropTypes.string.isRequired,
-};
-LoginComp.displayName = LoginComp;
+        this.state = {
+            username : '',
+            password : ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e) {
+        console.log('submitted');
+        const{username, password} = this.state;
+    }
+
+    handleChange(e) {
+        debugger;
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+    render() {
+        const { username, password } = this.state;
+        return(
+            <div>
+                <h2>{intl.get('menu/login').d('Login')}</h2>
+                <form className="loginWrapper" onSubmit={this.handleSubmit}>
+                    <section className="loginBgContainer"></section>
+                    <section className="loginContainer">
+                        <p> Username</p>
+                        <input type="text" placeholder="Username" onChange={this.handleChange}/>
+                        <p> Password</p>
+                        <input type="password" placeholder="Password" onChange={this.handleChange}/>
+                        <button> Login </button>
+                    </section>
+                </form>
+            </div>
+        );
+    }
+}
+
+// LoginComp.propTypes = {
+//   login: PropTypes.string.isRequired,
+// };
+// LoginComp.displayName = LoginComp;
 
 export { LoginComp as default };
