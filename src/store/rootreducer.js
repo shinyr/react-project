010b5a5c81018d0c6070b47948b12a1ab8cc7b25
shinyrajrl <shinyr@qburst.com>
2018-07-25 11:36:16 +0530

    Deleted unwanted files

--- src/store/rootreducer.js
+++ src/store/rootreducer.js
@@ -1,22 +1,10 @@
 import { combineReducers } from 'redux';
 
-/** shared reducers, keep here, not in page */
-import themes from './data/themes';
-import session from './data/session';
-import locale from './data/locale';
-
 /** individual reducers from each page */
 import home from '../pages/Home/data';
-import about from '../pages/About/data';
-import topics from '../pages/Topics/data';
 import login from '../pages/Login/data';
 
 export default combineReducers({
-  themes,
-  session,
-  locale,
   home,
-  about,
-  topics,
   login
 });
